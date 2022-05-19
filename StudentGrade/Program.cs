a7630d9a73fbc4edcccf41f887bf1dbdac898898
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-05-19 22:31:14 +0100

    Implemented student class

--- StudentGrade/Program.cs
+++ StudentGrade/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StudentGrade;
 
+
+Student student1 = new("Apshan", 'A', 1);
+student1.UpgradeGrade();
+
+Student student2 = new("Ashi", 'F', 5);
+student2.DowngradeGrade();
+
+Student student3 = new("Sarah", 'C', 6);
+student3.UpgradeGrade();
+
+Student student4 = new("Sithik", 'C', 4);
+student4.UpgradeGrade();
--- StudentGrade/Student.cs
+++ StudentGrade/Student.cs
@@ -1,11 +1,85 @@
-﻿using System;
+﻿
 namespace StudentGrade
 {
+
 	public class Student
 	{
-		public Student()
+		private string _name { get; set; }
+		private char _grade { get; set; }
+		private int _group;
+		private string _secretNickName = "MySecretNickName";
+
+		public Student(string name, char grade, int group)
 		{
+			_name = name;
+			_grade = grade;
+			_group = group;
+		}
+
+		public static char[] validGrades = { 'A', 'B', 'C', 'D', 'E', 'F' };
+		public static int[] validGroups = { 1, 2, 3, 4, 5, };
+
+
+		private static bool CheckValidGrade(char grade)
+		{
+			return validGrades.Contains(grade);
+		}
+
+		private static bool CheckValidGroup(int group)
+		{
+			return validGroups.Contains(group);
+		}
+
+		public void UpgradeGrade()
+		{
+			if (CheckValidGroup(_group))
+            {
+				if (_grade != 'A')
+				{
+
+					char UpgradedGrade = (char)(_grade - 1);
+					Console.WriteLine("The Upgraded grade for " + _name + " is " + UpgradedGrade);
+
+				}
+				else
+				{
+					Console.WriteLine("For " + _name + " We can't upgrade the grade more than 'A'");
+
+				}
+            }
+            else
+            {
+				Console.WriteLine(_name + " is not in valid Group");
+
+			}
+
+		}
+
+		public void  DowngradeGrade()
+		{
+			if (CheckValidGroup(_group))
+            {
+				if (_grade != 'F')
+				{
+					char DowngradedGrade = (char)(_grade + 1);
+					Console.WriteLine("The Downgraded grade for " + _name + " is " + DowngradedGrade);
+
+				}
+				else
+				{
+					Console.WriteLine("For " + _name + " We can't downgrade the grade more than 'F'");
+
+				}
+			}
+			else
+			{
+				Console.WriteLine(_name + " is not in valid Group");
+
+			}
+
+
+
 		}
 	}
-}
 
+}
